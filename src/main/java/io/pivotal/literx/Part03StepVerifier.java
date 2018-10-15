ea1f2ec70275012b88ae3c4e208e991fcf0fc8fb
Stephen Fox <sfox@sofi.org>
2018-10-15 17:09:55 -0600

    Complete exercise 4 and improve ex. 3

--- src/main/java/io/pivotal/literx/Part03StepVerifier.java
+++ src/main/java/io/pivotal/literx/Part03StepVerifier.java
@@ -77,14 +77,11 @@ public class Part03StepVerifier {
 
 //========================================================================================
 
-  // TODO Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
+  // Expect 3600 elements at intervals of 1 second, and verify quicker than 3600s
   // by manipulating virtual time thanks to StepVerifier#withVirtualTime, notice how long the test takes
   void expect3600Elements(Supplier<Flux<Long>> supplier) {
-    StepVerifier.withVirtualTime(supplier, () -> {
-      final VirtualTimeScheduler scheduler = VirtualTimeScheduler.create();
-      scheduler.advanceTimeBy(Duration.ofSeconds(3600));
-      return scheduler;
-    }, 3600)
+    StepVerifier.withVirtualTime(supplier)
+                .thenAwait(Duration.ofHours(1))
                 .expectNextCount(3600)
                 .verifyComplete();
   }
